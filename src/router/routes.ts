5f120b86076b37f12969475339937425994893e7
Euller Macena <49864491+eullerm@users.noreply.github.com>
2025-10-04 15:22:11 -0300

    Update routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,22 +2,29 @@ import {
   createRootRoute,
   createRoute,
   createRouter,
+  Outlet,
 } from "@tanstack/react-router";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 
 const rootRoute = createRootRoute({
-  component: Home,
+  component: Outlet,
   notFoundComponent: NotFound,
+});
+
+const homeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Home,
 });
 
 const notFoundRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: "*",
+  path: "/*",
   component: NotFound,
 });
 
-const routeTree = rootRoute.addChildren([notFoundRoute]);
+const routeTree = rootRoute.addChildren([homeRoute, notFoundRoute]);
 
 export const router = createRouter({
   routeTree,
